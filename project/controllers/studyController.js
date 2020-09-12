29b66b11b107afa04db53c6b2572eb199f495cd4
junhyeon <oh.junhyeon95@gmail.com>
2020-09-12 20:08:47 +0900

    123

--- project/controllers/studyController.js
+++ project/controllers/studyController.js
@@ -3,6 +3,14 @@ const StudyModel = require('../models/study');
 module.exports = {
     get: async ( req, res ) => {
         const id = req.params.id;
+        const {
+            junhyeon,
+            hyunjung,
+            yein
+        } = req.query
+
+        const token = req.headers.token
+
 
         const person = await StudyModel.studyPerson(id);
         
